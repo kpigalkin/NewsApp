13be6bd62dfb759181b196e82f578ab5407b737a
kpigalkin <kpigalkin@gmail.com>
2023-07-26 19:48:51 +0300

    Separated HTTPClient from making requests

--- NewsApp/Network/Base/HTTPClient.swift
+++ NewsApp/Network/Base/HTTPClient.swift
@@ -1,25 +1,11 @@
 import Foundation
 
 protocol HTTPClient {
-    func sendRequest(endpoint: Endpoint) async throws -> Data
+    func execute(request: URLRequest) async throws -> Data
 }
 
 extension HTTPClient {
-    func sendRequest(endpoint: Endpoint) async throws -> Data {
-        
-        var components = URLComponents()
-        components.scheme = endpoint.scheme
-        components.host = endpoint.host
-        components.path = endpoint.path
-        components.queryItems = endpoint.parameters
-        
-        guard let url = components.url else {
-            throw RequestError.invalidURL
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = endpoint.method.name
-        
+    func execute(request: URLRequest) async throws -> Data {
         do {
             let (data, response) = try await URLSession.shared.data(for: request)
             
--- NewsApp/Scenes/Home/Workers/HomeNetworkWorker.swift
+++ NewsApp/Scenes/Home/Workers/HomeNetworkWorker.swift
@@ -18,29 +18,54 @@ final class HomeNetworkWorker {}
 extension HomeNetworkWorker: HomeNetworkWorkingLogic, HTTPClient {
     
     func getNews(offset: Int) async throws -> NewsItems {
-        let data = try await sendRequest(endpoint: SpaceFlightNewsEndpoint.news(offset: offset))
+        let endpoint = SpaceFlightNewsEndpoint.news(offset: offset)
+        let request = try makeRequest(with: endpoint)
+        let data = try await execute(request: request)
         return try decode(NewsItems.self, from: data)
     }
     
     func getBlogs() async throws -> BlogItems {
-        let data = try await sendRequest(endpoint: SpaceFlightNewsEndpoint.blogs)
+        let endpoint = SpaceFlightNewsEndpoint.blogs
+        let request = try makeRequest(with: endpoint)
+        let data = try await execute(request: request)
         return try decode(BlogItems.self, from: data)
     }
     
     func getBlogDetail(id: Int) async throws -> Blog {
-        let data = try await sendRequest(endpoint: SpaceFlightNewsEndpoint.blogDetail(id: id))
+        let endpoint = SpaceFlightNewsEndpoint.blogDetail(id: id)
+        let request = try makeRequest(with: endpoint)
+        let data = try await execute(request: request)
         return try decode(Blog.self, from: data)
     }
     
     func getNewsDetail(id: Int) async throws -> News {
-        let data = try await sendRequest(endpoint: SpaceFlightNewsEndpoint.newsDetail(id: id))
+        let endpoint = SpaceFlightNewsEndpoint.newsDetail(id: id)
+        let request = try makeRequest(with: endpoint)
+        let data = try await execute(request: request)
         return try decode(News.self, from: data)
     }
 }
 
-    // MARK: - Decode
+    // MARK: - Decode & Request
 
 private extension HomeNetworkWorker {
+    func makeRequest(with endpoint: Endpoint) throws -> URLRequest {
+        var components = URLComponents()
+        components.scheme = endpoint.scheme
+        components.host = endpoint.host
+        components.path = endpoint.path
+        components.queryItems = endpoint.parameters
+        
+        guard let url = components.url else {
+            throw RequestError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.name
+        
+        return request
+    }
+    
     func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T {
         guard let decodedData = try? JSONDecoder().decode(type.self, from: data) else {
             throw RequestError.decode
